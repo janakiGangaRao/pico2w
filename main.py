d66bbbb274cc1a5335095d93bd75d9f6febb7eed
GangaRao <technocratpgm@gmail.com>
2026-06-01 19:53:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 # This is a test file of the OTA code
 
-import time
-
-# Use "LED" as the pin name for Pico W / Pico 2 W
-led = machine.Pin("LED", machine.Pin.OUT)
-
-while True:
-    led.value(1)  # Turn LED on
-    time.sleep(1)
-    led.value(0)  # Turn LED off
-    time.sleep(1)
+print("Hello World")
 
 
