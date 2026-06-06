9f52a316013297d6a397f8e4b124bdeafb830efc
GangaRao <technocratpgm@gmail.com>
2026-06-06 12:07:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from machine import Pin
 from time import sleep
 
 led = Pin("LED", Pin.OUT)
-print("[app] running v1 — slow blink")
+#print("[app] running v1 — slow blink")
 
 while True:
     led.toggle()
-    sleep(0.5)
-    print("I am Ready")
+    sleep(0.01)
+    
 
 
